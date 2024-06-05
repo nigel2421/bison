76bb10f9506d0e1d710827b66af0cfc76625d475
Nigel <79846205+nigel2421@users.noreply.github.com>
2024-06-05 16:35:17 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function ($) {
+"use strict";
+
+$(document).ready(function($){
+/*--
+	Mobile Menu
+------------------------*/
+$('.mobile-menu nav').meanmenu({
+	meanScreenWidth: "750",
+	meanMenuContainer: ".main-menu-wrap",
+});
+/*--
+	Scroll Up
+------------------------*/
+$.scrollUp({
+	easingType: 'linear',
+	scrollSpeed: 900,
+	animation: 'fade',
+	scrollText: '<i class="zmdi zmdi-chevron-up"></i>',
+});
+
+$('#demo-container').mixItUp();
+
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+	$('html, body').animate({
+	  scrollTop: target.offset().top
+	}, 1000);
+	return false;
+  }
+}
+});
+
+
+
+});	
+})(jQuery);	
